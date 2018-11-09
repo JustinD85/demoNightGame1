3bb56ab66893114b734b18cac8100bf2d44c0974
Matthew Foxwell <foxwellmw@gmail.com>
2018-11-08 17:03:11 -0700

    Add canvas classes

---
+++ js/makeProjectile.js
@@ -0,0 +1,13 @@
+class makeProjectile {
+    constructor() {
+        
+
+    // this.player = player || 1;
+    // if(this.player === 1) {
+        this.x = 20;
+        this.y = 70;
+        this.w = 50;
+        this.h = 50;
+        this.fill = 'red';
+    }
+}
