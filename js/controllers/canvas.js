7debd0a25aa25911130a471cc31116663c30fa16
Matthew Foxwell <foxwellmw@gmail.com>
2018-11-14 00:30:17 -0700

    Add basic melee atk

--- js/controllers/canvas.js
+++ js/controllers/canvas.js
@@ -1,9 +1,8 @@
-
 const canv = document.querySelector('canvas');
 
 let ctx = canv.getContext('2d');
-var projX = 200;
-var projY = 100;
+// var projX = 200;
+var projY = 100; //change to dynamic
 var direction = [0, 0];
 const p1 = 'player1';
 const p2 = 'player2';
@@ -12,7 +11,7 @@ if (canv.getContext) {
   var myRect = [];
   myRect.push(new makePlayer(p1));
   myRect.push(new makePlayer(p2));
- 
+
   for (var i in myRect) {
     oRec = myRect[i];
     ctx.fillStyle = oRec.fill;
@@ -23,7 +22,7 @@ const player1animation = myRect[0];
 const player2animation = myRect[1];
 
 // example casts
-playerCast(player1animation, player2animation);
+// playerCast(player1animation, player2animation);
 // playerCast(player2animation, player1animation);
 
 
@@ -37,26 +36,69 @@ function playerCast(attacker, defender) {
 }
 
 function castProjectile() {
-
   ctx.clearRect(projX, projY, 15, 15);
-  checkProjectilePosition(projX, projY, 15, 15);
   projX = projX + direction[0];
   ctx.fillStyle = 'red';
   ctx.fillRect(projX, projY, 15, 15);
   if (projX === projEndX) {
+    ctx.clearRect(projX, projY, 15, 15);
     return
-  } else { 
-  requestAnimationFrame(castProjectile);
+  } else {
+    requestAnimationFrame(castProjectile);
   }
 }
 
-function checkProjectilePosition(proXVel, projYVel, projSizeX, projSizeY) {
-  const canvWidth = canv.width;
-  const canvHeight = canv.height;
-  if (proXVel + projSizeX === canvWidth) {
-    projectileDirection(proXVel, projYVel)
+function playerWeakAttack(attacker, defender) {
+  attacker.weakMeleeAttack(defender);
+  attackerStartX = attacker.x;
+  attackerStartY = attacker.y;
+  attackerDirection = attacker.direction;
+  attackerEndX = attacker.endX;
+  attackerPlayer = attacker.player;
+  attackerSpeed = attacker.speed + 1;
+  requestAnimationFrame(castWeakAttack);
+}
+
+function castWeakAttack() {
+
+  ctx.clearRect(attackerStartX, attackerStartY, 50, 50);
+  attackerStartX = attackerStartX + attackerDirection[0] + attackerSpeed;
+  if (attackerPlayer === 'player1') {
+    ctx.fillStyle = 'blue';
+  } else {
+    ctx.fillStyle = 'green';
+  }
+
+  ctx.fillRect(attackerStartX, attackerStartY, 50, 50);
+  if (attackerPlayer === 'player1' && attackerStartX > attackerEndX) {
+    ctx.clearRect(attackerStartX, attackerStartY, 50, 50);
+    return
+  } else if (attackerPlayer === 'player2' && attackerStartX < attackerEndX) {
+    ctx.clearRect(attackerStartX, attackerStartY, 50, 50);
+    return
+  } else {
+    // if (attackerSpeed < 5) {
+    // attackerSpeed = attackerSpeed + 1;
+    // }
+    requestAnimationFrame(castWeakAttack);
   }
 }
+
+// example weak attack
+playerWeakAttack(player1animation, player2animation);
+
+
+
+
+
+
+// function checkProjectilePosition(proXVel, projYVel, projSizeX, projSizeY) {
+//   const canvWidth = canv.width;
+//   const canvHeight = canv.height;
+//   if (proXVel + projSizeX === canvWidth) {
+//     projectileDirection(proXVel, projYVel)
+//   }
+// }
 
 
 // function projectileDirection(proXVel, projYVel) {
@@ -67,5 +109,4 @@ function checkProjectilePosition(proXVel, projYVel, projSizeX, projSizeY) {
 //     return direction;
 //   }
 // }
-
  
--- js/controllers/makePlayer.js
+++ js/controllers/makePlayer.js
@@ -1,41 +1,57 @@
 class makePlayer {
-    constructor(player) {
-    if(player === 'player1') {
-        this.player = 'player1';
-        this.x = 20;
-        this.fill = 'blue';
-    } else {
-        this.player = 'player2';
-        this.x = 200;
-        this.fill = 'green';
-    }
-    this.y = 70;
-    this.w = 50;
-    this.h = 50;
-    this.startX = 0;
-    this.startY = 0;
-    this.endX = 0;
-    this.endY = 0;
-    this.direction = [];
-    
-    }
-
-    createProjectile(defender) {
-     if (this.player === 'player1') {
-              
-        this.startX = this.x + this.w;
-        this.startY = this.y/2;
-        this.endX = defender.x -15;
-        this.endY = this.startY;
-        this.direction = [1,0]
-       
-    } else {
-        this.startX = this.x - 15;
-        this.startY = this.y/2;
-        this.endX = defender.x + defender.w;
-        this.endY = this.startY;
-        this.direction = [-1,0]
-    }
-
-}
+	constructor(player) {
+		if (player === 'player1') {
+			this.player = 'player1';
+			this.x = 20;
+			this.fill = 'blue';
+		} else {
+			this.player = 'player2';
+			this.x = 230;
+			this.fill = 'green';
+		}
+		this.y = 70;
+		this.w = 50;
+		this.h = 50;
+		this.startX = 0;
+		this.startY = 0;
+		this.endX = 0;
+		this.endY = 0;
+		this.direction = [];
+		this.speed = 0;
+		
+	}
+	
+	createProjectile(defender) {
+		if (this.player === 'player1') {
+			this.startX = this.x + this.w;
+			this.startY = this.y / 2;
+			this.endX = defender.x - 15;
+			this.endY = this.startY;
+			this.direction = [1, 0]
+			
+		} else {
+			this.startX = this.x - 15;
+			this.startY = this.y / 2;
+			this.endX = defender.x + defender.w;
+			this.endY = this.startY;
+			this.direction = [-1, 0]
+		}
+	}
+	
+	weakMeleeAttack(defender) {
+		if (this.player === 'player1') {
+			this.startX = this.x + this.w;
+			this.startY = this.y / 2;
+			this.endX = defender.x - 50;
+			this.endY = this.startY;
+			this.direction = [1, 0]
+			
+		} else {
+			this.startX = this.x - 50;
+			this.startY = this.y / 2;
+			this.endX = defender.x + defender.w;
+			this.endY = this.startY;
+			this.direction = [-1, 0]
+		}
+	}
 }
